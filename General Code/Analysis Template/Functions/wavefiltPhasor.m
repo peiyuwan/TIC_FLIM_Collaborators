2ec239ead5ca7fe43d5cae4cd90cf0a555cfdbf7
peiyuwan <31325690+peiyuwan@users.noreply.github.com>
2021-08-23 23:19:40 -0700

    deleted unncecssary data

--- General Code/Analysis Template/Functions/wavefiltPhasor.m
+++
@@ -1,7 +0,0 @@
-%% Wavelet Filtering of the structure. 
-function filt_struct = wavefiltPhasor(org_struct)
-filt_G = wdenoise2(org_struct.G);
-filt_S = wdenoise2(org_struct.S);
-
-filt_struct = struct('int',org_struct.int,'G',filt_G,'S',filt_S);
-end 
